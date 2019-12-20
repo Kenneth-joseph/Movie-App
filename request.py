858f2343efc680918a4b8ad1a77e4022d886a95b
Kenneth-joseph <josephochieng812@gmail.com>
2019-12-20 18:02:00 +0300

    edited my movie class and its test

--- request.py
+++ request.py
@@ -1,6 +1,13 @@
 from app import app
 from instance.config import MOVIE_API_KEY
+from models import movie
+import urllib.request,json
+
+Movie=movie.Movie
 
 # getting API key
 
-api_key = app.config[MOVIE_API_KEY]
+api_key = app.config['MOVIE_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["MOVIE_API_BASE_URL"]
