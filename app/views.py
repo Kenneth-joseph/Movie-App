5a47e035c37eeea6c8a1db43dff24f907edf3318
Kenneth-joseph <josephochieng812@gmail.com>
2020-01-10 15:41:47 +0300

    added my css and bootstrap

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from .request import get_movies
+from .request import get_movie
 
 
 @app.route('/')
@@ -18,10 +19,12 @@ def index():
     return render_template('index.html', message=message, title=title, popular=popular_movies,upcoming=upcoming_movie,now_showing=now_showing_movie)
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
     """
       View root page function that returns the index page and its data
     """
-    title = movie_id
-    return render_template('movie.html', title=title)
+    movie=get_movie(id)
+    title=f'{movie.title}'
+
+    return render_template('movie.html', title=title,movie=movie)
